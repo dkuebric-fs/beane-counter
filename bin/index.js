e0006b38d58d58f4acfda53e7c03619137dae760
MattW <mattwear@fullstory.com>
2021-07-09 16:42:09 -0500

    added more functionality

--- bin/index.js
+++ bin/index.js
@@ -3,6 +3,7 @@
 const yargs = require('yargs');
 const github = require('../src/api/github.js');
 const dotenv = require('dotenv');
+const chalk = require("chalk");
 
 dotenv.config()
 
@@ -19,10 +20,21 @@ console.log(`Searching PRs for ${options.author}`);
 
 const searchApi = async () => {
     try {
-        const response = await github.client.get(`search/issues?q=author:${options.author}`);
-    
+        const authored = await github.client.get(`search/issues?q=author:${options.author}&sort:updated-des`);
+        const commeted = await github.client.get(`search/issues?q=commenter:${options.author}&sort:updated-des`);
 
-        console.log(response.data); 
+        const authoredHeader = chalk.green.bold("Last 20 PRs Authored!");
+        const authoredPRsToPrint = authored.data.items.slice(0, 20).map(pr => `Created ${Math.round((new Date().getTime() - new Date(pr.created_at).getTime()) / (1000 * 3600 * 24))} days go, ${pr.comments} Comments, url: ${pr.html_url}`);
+        console.log(authoredHeader); 
+        console.log(authoredPRsToPrint.join("\n"));
+
+        console.log("\n\n");
+
+        const commentedHeader = chalk.green.bold("Last 20 PRs Commented On!");
+        const commentedPRsToPrint = commeted.data.items.slice(0, 20).map(pr => `Updated ${Math.round((new Date().getTime() - new Date(pr.updated_at).getTime()) / (1000 * 3600 * 24))} days go, url -> ${pr.html_url}`);
+        console.log(commentedHeader); 
+        console.log(commentedPRsToPrint.join("\n"));
+
     } catch (e) {
         console.log(e);
     }
