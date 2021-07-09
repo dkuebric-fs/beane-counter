2879bb96a91ed71892a284d65ff975a65cdecbd6
MattW <mattwear@fullstory.com>
2021-07-09 10:10:15 -0500

    clean up some code

--- bin/index.js
+++ bin/index.js
@@ -9,9 +9,6 @@ dotenv.config()
 const AUTHORS = JSON.parse(process.env.GITHUB_AUTHORS);
 const REPOS = JSON.parse(process.env.GITHUB_REPOS);
 
-//console.log(AUTHORS);
-//console.log(REPOS);
-
 const options = yargs
  .usage("Usage: -a <author>")
  .option("a", { alias: "author", choices: AUTHORS, describe: "GitHub Author", type: "string", demandOption: true })
@@ -24,12 +21,11 @@ const searchApi = async () => {
     try {
         const response = await github.client.get(`search/issues?q=author:${options.author}`);
     
-        console.log(response.data.total_count); 
+
+        console.log(response.data); 
     } catch (e) {
         console.log(e);
     }
 };
 
 searchApi();
-
-//export default () => {}
--- src/api/github.js
+++ src/api/github.js
@@ -2,7 +2,7 @@ const axios = require('axios').default;
 const dotenv = require('dotenv');
 
 dotenv.config()
-//console.log(process.env.GITHUB_BASE_URL);
+
 module.exports = {
     client: axios.create({
         baseURL: `${process.env.GITHUB_BASE_URL}`,
